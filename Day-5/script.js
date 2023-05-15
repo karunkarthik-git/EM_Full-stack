71d74091bf19f19e12ac44da80ad9d7d5d57fa35
karunkarthik-git <kkarun0542@gmail.com>
2023-05-15 22:05:47 +0530

    Changes to Support Day-5

---
+++ Day-5/script.js
@@ -0,0 +1,33 @@
+let a = 5;
+let b = 10;
+// console.log('Logging from file')
+// Everything in Js is an object.
+// const obj = { 'key':'value' };
+
+function doSomething() {
+    let k = 100;
+    console.log('Did something');
+}
+
+doSomething();
+
+/*
+    How does the code gets executed in the Js?
+
+    Js -> Js helps in making the websites dynamic. Js can only run on the browser.
+    (Node.js)
+
+    1. Memory Execution. -> It will check the amount of memory the whole program would consume.
+                            a -> undefined.
+                            b -> undefined.
+    2. Code Execution. -> It will assign the values to the variables that are created after the 1st step.
+                            a -> 5.
+                            b -> 10.
+
+*/
+
+/**
+    returnType functionName(datatype arguments) {
+
+    }
+ */
